59b85fc1490e44e5f8ea2178417f552167ab103b
cem256 <cemo99@gmail.com>
2023-05-15 23:20:22 +0300

    refactor pending view

--- lib/feature/pending/presentation/view/pending_view.dart
+++ lib/feature/pending/presentation/view/pending_view.dart
@@ -1,5 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:winmeet_mobile/app/utils/calendar/calendar_utils.dart';
 import 'package:winmeet_mobile/app/widgets/text/winmeet_body_large.dart';
 import 'package:winmeet_mobile/app/widgets/text/winmeet_title_large.dart';
 import 'package:winmeet_mobile/app/widgets/text/winmeet_title_medium.dart';
@@ -39,116 +42,155 @@ class _PendingViewBody extends StatelessWidget {
           case PageStatus.loading:
             return const CircularProgressIndicator.adaptive();
           case PageStatus.success:
-            return Padding(
-              padding: context.paddingAllDefault,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (state.events.isEmpty)
-                    const Center(
-                      child: WinMeetBodyLarge(
-                        text: 'No Meetings Pending',
-                      ),
-                    )
-                  else
-                    Expanded(
-                      child: ListView.separated(
-                        separatorBuilder: (context, index) => SizedBox(
-                          height: context.mediumValue,
-                        ),
-                        itemCount: state.events.length,
-                        itemBuilder: (context, index) {
-                          final eventDates = state.events[index].getEventStartDates();
-                          return Card(
-                            child: Padding(
-                              padding: context.paddingAllLow,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  WinMeetTitleLarge(
-                                    text: state.events[index].eventName,
-                                  ),
-                                  ListView.separated(
-                                    physics: const NeverScrollableScrollPhysics(),
-                                    shrinkWrap: true,
-                                    separatorBuilder: (context, index) => SizedBox(
-                                      height: context.lowValue,
-                                    ),
-                                    itemCount: eventDates.length,
-                                    itemBuilder: (context, dateIndex) => Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        WinMeetTitleMedium(text: 'Date ${dateIndex + 1}'),
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Column(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  'Ends: ${DateFormatUtils.getMonthDayYearHour(eventDates[dateIndex].eventEndDate)}',
-                                                ),
-                                                Text(
-                                                  'Starts: ${DateFormatUtils.getMonthDayYearHour(eventDates[dateIndex].eventStartDate)}',
-                                                ),
-                                              ],
-                                            ),
-                                            TextButton.icon(
-                                              onPressed: () async {
-                                                await context.read<PendingCubit>().voteMeetingDate(
-                                                      eventId: state.events[index].id,
-                                                      dateIndex: eventDates[index].index,
-                                                    );
-                                              },
-                                              label: const Text('Vote'),
-                                              icon: const Icon(Icons.check),
-                                            ),
-                                          ],
-                                        ),
-                                      ].withSpaceBetween(
-                                        height: context.lowValue,
-                                      ),
-                                    ),
-                                  ),
-                                ].withSpaceBetween(
-                                  height: context.lowValue,
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    )
-                ],
-              ),
-            );
-
+            return _SuccessWidget(state: state);
           case PageStatus.failure:
-            return Padding(
-              padding: context.paddingAllDefault,
-              child: Column(
-                children: [
-                  const WinMeetBodyLarge(
-                    text: 'Something Went Wrong',
-                  ),
-                  Row(
-                    children: [
-                      const Spacer(),
-                      Expanded(
-                        child: ElevatedButton.icon(
-                          label: const Text('Retry'),
-                          icon: const Icon(Icons.refresh),
-                          onPressed: () => context.read<PendingCubit>().getPendingMeetings(),
-                        ),
-                      ),
-                      const Spacer()
-                    ],
-                  ),
-                ].withSpaceBetween(height: context.highValue),
-              ),
-            );
+            return const _FailureWidget();
         }
       },
+    );
+  }
+}
+
+class _SuccessWidget extends StatelessWidget {
+  const _SuccessWidget({required this.state});
+
+  final PendingState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: context.paddingAllDefault,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (state.events.isEmpty)
+            const Center(
+              child: WinMeetBodyLarge(
+                text: 'No Meetings Pending',
+              ),
+            )
+          else
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (context, index) => SizedBox(
+                  height: context.mediumValue,
+                ),
+                itemCount: state.events.length,
+                itemBuilder: (context, index) {
+                  return _EventDetailsWidget(state: state, index: index);
+                },
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _EventDetailsWidget extends StatelessWidget {
+  const _EventDetailsWidget({required this.state, required this.index});
+
+  final PendingState state;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final eventDates = state.events[index].getEventStartDates();
+    return Card(
+      child: Padding(
+        padding: context.paddingAllLow,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                WinMeetTitleLarge(
+                  text: state.events[index].eventName,
+                ),
+                if (state.events[index].voteDueDate.isAtSameMomentAs(CalendarUtils.today) ||
+                    state.events[index].voteDueDate.isAfter(CalendarUtils.today))
+                  Text(
+                    'Ends in ${timeago.format(state.events[index].voteDueDate, allowFromNow: true, locale: 'en_short')}',
+                  )
+              ],
+            ),
+            SizedBox(
+              height: context.lowValue,
+            ),
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: eventDates.length,
+              itemBuilder: (context, dateIndex) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    WinMeetTitleMedium(text: 'Date ${dateIndex + 1}'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Starts: ${DateFormatUtils.getMonthDayYearHour(eventDates[dateIndex].eventStartDate)}',
+                            ),
+                            Text(
+                              'Ends: ${DateFormatUtils.getMonthDayYearHour(eventDates[dateIndex].eventEndDate)}',
+                            ),
+                          ],
+                        ),
+                        TextButton.icon(
+                          onPressed: () async {
+                            await context.read<PendingCubit>().voteMeetingDate(
+                                  eventId: state.events[index].id,
+                                  dateIndex: eventDates[dateIndex].index,
+                                );
+                          },
+                          label: const Text('Vote'),
+                          icon: const Icon(Icons.check),
+                        )
+                      ],
+                    ),
+                  ],
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _FailureWidget extends StatelessWidget {
+  const _FailureWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: context.paddingAllDefault,
+      child: Column(
+        children: [
+          const WinMeetBodyLarge(
+            text: 'Something Went Wrong',
+          ),
+          Row(
+            children: [
+              const Spacer(),
+              Expanded(
+                child: ElevatedButton.icon(
+                  label: const Text('Retry'),
+                  icon: const Icon(Icons.refresh),
+                  onPressed: () => context.read<PendingCubit>().getPendingMeetings(),
+                ),
+              ),
+              const Spacer()
+            ],
+          ),
+        ].withSpaceBetween(height: context.highValue),
+      ),
     );
   }
 }
